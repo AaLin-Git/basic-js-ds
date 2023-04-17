72a2c6b0b6b40f5e1f460be274280aebfc4c1759
AaLin-Git <aalin.box.it@gmail.com>
2023-04-17 23:19:25 +0200

    feat: write add and has methods

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,77 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
+    return this.root;
+  }
+
+  add(data) {
+    this.root = addTo(this.root, data);
+
+    function addTo(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data < node.data) {
+        node.left = addTo(node.left, data);
+      } else {
+        node.right = addTo(node.right, data);
+      }
+      return node;
+    }
+  }
+
+  has(data) {
+    return BinarySearchTree(this.root, data);
+    function search(node, value) {
+      if(!node) {
+        return false;
+      }
+      if(node.data === data) {
+        return true;
+      }
+
+      if( data < node.data) {
+        return search(node.left, data);
+      } else {
+        return search(node.right, data);
+      }
+    }
+  }
+
+  find(data) {
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+  }
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
